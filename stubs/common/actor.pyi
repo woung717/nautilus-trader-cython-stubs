9188283ee02f2528663f9368d947c0885808e0c9
HyeonUng Shin <woung717@gmail.com>
2025-10-29 16:25:52 +1100

    Fixed issue #1
    Changed the 'start' parameter in request_bars to be required.

--- stubs/common/actor.pyi
+++ stubs/common/actor.pyi
@@ -1827,7 +1827,7 @@ class Actor(Component):
     def request_bars(
         self,
         bar_type: BarType,
-        start: dt.datetime | None = None,
+        start: dt.datetime,
         end: dt.datetime | None = None,
         limit: int = 0,
         client_id: ClientId | None = None,
